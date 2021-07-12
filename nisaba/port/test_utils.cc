4bce74d5f6dd4fb47d4430014d5533a59318d7b3
Alexander Gutkin <agutkin@google.com>
2021-07-12 07:31:38 -0700

    Moving test utils from MozoLM.
    PiperOrigin-RevId: 384229976

---
+++ nisaba/port/test_utils.cc
@@ -0,0 +1,43 @@
+// Copyright 2021 Nisaba Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "nisaba/port/test_utils.h"
+
+#include <filesystem>
+
+#include "fst/compat.h"
+#include "gtest/gtest.h"
+#include "absl/flags/flag.h"
+#include "nisaba/port/file_util.h"
+
+using nisaba::file::GetRunfilesResourcePath;
+using nisaba::file::JoinPath;
+
+namespace nisaba {
+namespace testing {
+
+std::string TestFilePath(std::string_view dir_name,
+                         std::string_view file_name) {
+  const auto status = GetRunfilesResourcePath(JoinPath(dir_name, file_name));
+  std::filesystem::path model_path;
+  if (status.ok()) {
+    model_path = std::move(status.value());
+  } else {
+    LOG(WARNING) << "Can't deduce runfiles path";
+  }
+  return model_path.make_preferred().string();
+}
+
+}  // namespace testing
+}  // namespace nisaba
---
+++ nisaba/port/test_utils.h
@@ -0,0 +1,32 @@
+// Copyright 2021 Nisaba Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Collection of small utilities for use in tests.
+
+#ifndef NISABA_PORT_TEST_UTILS_H_
+#define NISABA_PORT_TEST_UTILS_H_
+
+#include <string>
+#include <string_view>
+
+namespace nisaba {
+namespace testing {
+
+// Returns path to a file in a given directory under a test directory tree.
+std::string TestFilePath(std::string_view dir_name, std::string_view file_name);
+
+}  // namespace testing
+}  // namespace nisaba
+
+#endif  // NISABA_PORT_TEST_UTILS_H_
