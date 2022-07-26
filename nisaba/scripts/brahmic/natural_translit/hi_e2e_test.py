54985401708c6aefce062e1862cdf3bf86c366ac
Isin Demirsahin <isin@google.com>
2022-07-26 08:44:01 -0700

    Move hi_e2e tests to hi_e2e_test.py
    PiperOrigin-RevId: 463345151

---
+++ nisaba/scripts/brahmic/natural_translit/hi_e2e_test.py
@@ -0,0 +1,47 @@
+# Copyright 2022 Nisaba Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for hi_e2e."""
+
+from absl.testing import absltest
+from nisaba.scripts.brahmic.natural_translit import hi_e2e
+from nisaba.scripts.utils import test_util
+
+
+_TEST_CASES = [
+    (hi_e2e.iso_to_psaf(), [
+        ('hiṁdī', 'hindii'),
+        ('hindī', 'hindii'),
+        ('iṁḍiyā', 'indiyaa'),
+        ('caṁbā', 'chambaa'),
+        ('haḍḍiyōṁ', 'haddiyoon'),
+        ('ajñāna', 'agyaana'),
+    ]),
+    (hi_e2e.iso_to_psac(), [
+        ('hiṁdī', 'hindi'),
+        ('hindī', 'hindi'),
+        ('iṁḍiyā', 'indiya'),
+        ('ajñāna', 'agyana'),
+    ]),
+]
+
+
+class HiE2ETest(test_util.FstTestCase):
+
+  def test_all(self):
+    self.assertFstStrIoTestCases(_TEST_CASES)
+
+
+if __name__ == '__main__':
+  absltest.main()
