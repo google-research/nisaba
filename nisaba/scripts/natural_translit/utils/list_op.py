1cb2a5c26a9840788174f3375b48d9d58ede2aaf
Isin Demirsahin <isin@google.com>
2023-11-02 05:57:27 -0700

    Add shorthands for making a list from an attribute of a list of objects with type_check, and for applying union and star to fst lists from an attribute.
    PiperOrigin-RevId: 578825124

--- nisaba/scripts/natural_translit/utils/list_op.py
+++ nisaba/scripts/natural_translit/utils/list_op.py
@@ -13,10 +13,11 @@
 # limitations under the License.
 
 # Lint as: python3
-"""Common constants and symbol and alignment forming functions."""
+"""Shorthands for functions that loop over lists of arguments."""
 
-from typing import Callable, List, Tuple
+from typing import Any, Callable, List
 import pynini as pyn
+from nisaba.scripts.natural_translit.utils import type_op as ty
 
 # Shorthands for optimized union and star.
 
@@ -25,6 +26,7 @@ def star_opt(fst: pyn.Fst) -> pyn.Fst:
   return fst.star.optimize()
 
 
+# TODO: Rename this union() to be consistent with the following functions.
 def union_opt(*fsts) -> pyn.Fst:
   return pyn.union(*fsts).optimize()
 
@@ -36,8 +38,9 @@ def union_star(*fsts) -> pyn.Fst:
 
 
 def apply_foreach(
-    func, arg_list: Tuple[Callable[[], pyn.Fst], List[List[pyn.Fst]]]
-    ) -> List[pyn.Fst]:
+    func: Callable[..., Any],
+    arg_list: List[List[Any]]
+) -> List[Any]:
   """Lists the returns of a function from a list of arguments.
 
   Args:
@@ -68,27 +71,65 @@ def apply_foreach(
 
 
 def apply_union(
-    func, arg_list: Tuple[Callable[[], pyn.Fst], List[List[pyn.Fst]]]
-    ) -> pyn.Fst:
+    func: Callable[..., ty.FstLike],
+    arg_list: List[List[Any]]
+) -> ty.pyn.Fst:
   return union_opt(*apply_foreach(func, arg_list))
 
 
 def apply_union_star(
-    func, arg_list: Tuple[Callable[[], pyn.Fst], List[List[pyn.Fst]]]
-    ) -> pyn.Fst:
-  return star_opt(apply_union(func, arg_list))
+    func: Callable[..., ty.FstLike],
+    arg_list: List[List[Any]]
+) -> ty.pyn.Fst:
+  return union_star(*apply_foreach(func, arg_list))
 
-# Special cases where the function is a simple pyn.cross(old, new)
+# Special cases where the function is a simple ty.pyn.cross(old, new)
 
 
 def cross_union(
-    arg_list: List[Tuple[pyn.Fst, pyn.Fst]]
-    ) -> pyn.Fst:
+    arg_list: List[List[pyn.FstLike]]
+) -> pyn.Fst:
   """Shorthand for pyn.union(pyn.cross(x, y), pyn.cross(y, z),...)"""
   return union_opt(*apply_foreach(pyn.cross, arg_list))
 
 
 def cross_union_star(
-    arg_list: List[Tuple[pyn.Fst, pyn.Fst]]
-    ) -> pyn.Fst:
-  return star_opt(cross_union(arg_list))
+    arg_list: List[List[pyn.FstLike]]
+) -> pyn.Fst:
+  return union_star(*apply_foreach(pyn.cross, arg_list))
+
+
+def attr_list(
+    things: list[Any],
+    attr: str,
+    attr_type: ty.TypeOrNothing = ty.UNSPECIFIED
+) -> list[Any]:
+  """Makes a list from an attribute of a list of objects.
+
+  Args:
+    things: A list of objects.
+    attr: The attribute from which the new list is to be compiled. If the
+      object doesn't have the requested attribute, skips that object.
+    attr_type: If specified, only the results that are instances of this type
+      will be added to the list.
+
+  Returns:
+    if attr_type is specified:
+      [t.attr for t in things if isinstance(t.attr, attr_type)]
+    else:
+      [t.attr for t in things]
+  """
+  l = [ty.get_attribute(t, attr, want=attr_type) for t in things]
+  return list(filter(ty.is_found, l))
+
+
+def fst_attr_list(things: list[Any], attr: str) -> list[pyn.Fst]:
+  return attr_list(things, attr, ty.FstLike)
+
+
+def fst_attr_list_union(things: list[Any], attr: str) -> pyn.Fst:
+  return union_opt(*fst_attr_list(things, attr))
+
+
+def fst_attr_list_union_star(things: list[Any], attr: str) -> pyn.Fst:
+  return union_star(*fst_attr_list(things, attr))
