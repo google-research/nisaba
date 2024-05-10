85a01b5f6fcd644af46de2df3c2c19643aacde23
Isin Demirsahin <isin@google.com>
2024-05-10 02:35:01 -0700

    Node and Lattice classes for accessing FST states by coordinates.
    PiperOrigin-RevId: 632429887

---
+++ nisaba/scripts/natural_translit/utils/lattice.py
@@ -0,0 +1,171 @@
+# Copyright 2024 Nisaba Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Lattice class for building aligner FSTs."""
+
+import pynini as pyn
+from nisaba.scripts.natural_translit.utils import log_op as log
+from nisaba.scripts.natural_translit.utils import type_op as ty
+
+
+class Node(ty.Thing):
+  """A node in a Lattice that holds an FST state.
+
+  Attributes:
+    x: The x-coordinate of the node on a Lattice
+    y: The y-coordinate of the node on a Lattice.
+    fst: The FST that the state of the node belongs to.
+    state: The label of the  FST state this node holds as int, or ty.UNASSIGNED.
+
+  The default coordinates of a Node that is not initiated by a lattice, such as
+    a free-standing Node that holds the error state of an FST, are (-1, -1).
+
+  Multiple nodes can be assigned the same state, so that the same state can be
+    accessed by different coordinates on intersecting lattices.
+  """
+
+  def __init__(
+      self,
+      alias: str,
+      text: str = '',
+      x: int = -1,
+      y: int = -1,
+      fst: pyn.Fst = pyn.Fst(),
+  ):
+    super().__init__(alias, text)
+    self.x = x
+    self.y = y
+    self.fst = fst
+    self.state = ty.UNASSIGNED
+
+  def __str__(self):
+    return self.text
+
+  def has_state(self) -> bool:
+    return isinstance(self.state, int)
+
+  def state_str(self) -> str:
+    if self.has_state():
+      return str(self.state)
+    else:
+      return '∅'
+
+
+class Lattice(ty.Thing):
+  """A 2 dimensional lattice for accessing FST states by x and y coordinates.
+
+  Attributes:
+    fst: The FST associated with the lattice.
+    width: The width of the lattice.
+    height: The height of the lattice.
+    nodes: A list of lists of nodes.
+    error: The node that will hold the error state of the FST.
+  """
+
+  def __init__(
+      self,
+      alias: str,
+      width: int,
+      height: int,
+      fst: pyn.Fst = pyn.Fst(),
+      error: Node = Node('error'),
+      populate: bool = False,
+  ):
+    super().__init__(alias)
+    self.text = alias
+    self.fst = fst
+    self.width = width
+    self.height = height
+    self._nodes = [
+        [self._new_node(x, y) for x in range(self.width)]
+        for y in range(self.height)
+    ]
+    self.error = error
+    if error.fst is not self.fst:
+      error.fst = self.fst
+    if not self.error.has_state():
+      self.error.state = self.fst.add_state()
+    if populate:
+      for row in self._nodes:
+        for node in row:
+          node.state = self.fst.add_state()
+
+  def __str__(self):
+    return self.text
+
+  def nodes_str(self) -> str:
+    return 'Lattice: %s\nNodes:\n%s\n' % (
+        self.alias,
+        '\n'.join(
+            ['\t'.join([node.text for node in row]) for row in self._nodes]
+        ),
+    )
+
+  def states_str(self) -> str:
+    return 'Lattice: %s\nStates:\n%s\n' % (
+        self.alias,
+        '\n'.join([
+            '\t'.join([node.state_str() for node in row]) for row in self._nodes
+        ]),
+    )
+
+  def _new_node(self, x: int, y: int, populate: bool = False) -> Node:
+    """Makes a new node at the given coordinates."""
+    alias = '%s_node_%d_%d' % (self.alias, x, y)
+    text = '%s(%d,%d)' % (self.text, x, y)
+    node = Node(alias, text, x, y, self.fst)
+    if populate:
+      node.state = self.fst.add_state()
+    return node
+
+  def node(self, x: int, y: int) -> Node:
+    """Returns the Node at (x, y).
+
+    If x or y are out of bounds, returns the error node.
+
+    Args:
+      x: The x-coordinate of the node.
+      y: The y-coordinate of the node.
+
+    Returns:
+      Node.
+    """
+    if x in range(self.width) and y in range(self.height):
+      return self._nodes[y][x]
+    return self.error
+
+  def state(self, x: int, y: int) -> int:
+    """Returns the state of the Node at (x, y).
+
+    If the Node has no state, adds a state to the FST and assigns it to the
+    node. If x or y are out of bounds, returns the state of the error node.
+
+    Args:
+      x: The x-coordinate of the node.
+      y: The y-coordinate of the node.
+
+    Returns:
+      The label of the state of the node at (x, y) as int.
+    """
+    node = self.node(x, y)
+    if not node.has_state():
+      node.state = self.fst.add_state()
+    if isinstance(node.state, int):
+      return log.dbg_return(node.state)
+    else:
+      return log.dbg_return(hash(node), 'unassigned state')
+
+  def state_of(self, node: Node) -> int:
+    """Returns the state of a Node. Adds a new state if needed."""
+    return self.state(node.x, node.y)
---
+++ nisaba/scripts/natural_translit/utils/lattice_test.py
@@ -0,0 +1,70 @@
+# Copyright 2024 Nisaba Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from nisaba.scripts.natural_translit.utils import lattice as lt
+
+
+class LatticeTest(absltest.TestCase):
+
+  def test_node(self):
+    node = lt.Node('test', 'test_node', 1, 2)
+    self.assertEqual(node.alias, 'test')
+    self.assertEqual(str(node), 'test_node')
+    self.assertEqual(node.x, 1)
+    self.assertEqual(node.y, 2)
+    self.assertFalse(node.has_state())
+    self.assertEqual(node.state_str(), '∅')
+
+  def test_lattice(self):
+    lattice = lt.Lattice('test', 6, 4)
+    self.assertEqual(lattice.alias, 'test')
+    self.assertIs(lattice.fst, lattice.error.fst)
+    self.assertEqual(
+        lattice.nodes_str(),
+        'Lattice: test\n'
+        'Nodes:\n'
+        'test(0,0)	test(1,0)	test(2,0)	test(3,0)	test(4,0)	test(5,0)\n'
+        'test(0,1)	test(1,1)	test(2,1)	test(3,1)	test(4,1)	test(5,1)\n'
+        'test(0,2)	test(1,2)	test(2,2)	test(3,2)	test(4,2)	test(5,2)\n'
+        'test(0,3)	test(1,3)	test(2,3)	test(3,3)	test(4,3)	test(5,3)\n'
+    )
+    self.assertEqual(
+        lattice.states_str(),
+        'Lattice: test\n'
+        'States:\n'
+        '∅	∅	∅	∅	∅	∅\n'
+        '∅	∅	∅	∅	∅	∅\n'
+        '∅	∅	∅	∅	∅	∅\n'
+        '∅	∅	∅	∅	∅	∅\n'
+    )
+    self.assertIs(lattice.node(7, 9), lattice.error)
+    self.assertIs(lattice.state(7, 9), lattice.error.state)
+
+  def test_states(self):
+    lattice = lt.Lattice('test', 4, 3, populate=True)
+    self.assertEqual(lattice.error.state_str(), '0')
+    self.assertEqual(
+        lattice.states_str(),
+        'Lattice: test\n'
+        'States:\n'
+        '1	2	3	4\n'
+        '5	6	7	8\n'
+        '9	10	11	12\n'
+    )
+    self.assertEqual(lattice.state_of(lattice.node(1, 2)), 10)
+
+
+if __name__ == '__main__':
+  absltest.main()
